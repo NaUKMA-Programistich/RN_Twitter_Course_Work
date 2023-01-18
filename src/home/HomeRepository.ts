557adb6fa31480646e5005f64fd11ba1481fbbc6
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2023-01-18 13:09:49 +0200

    fix params

--- src/home/HomeRepository.ts
+++ src/home/HomeRepository.ts
@@ -1,19 +1,27 @@
 import {getOauthHeader} from '../auth/AuthStorage';
 
 export const getHomeTimeline = async () => {
+  const params = new URLSearchParams({
+    count: '200',
+    include_entities: 'true',
+    exclude_replies: 'false',
+  });
+  const url =
+    'https://api.twitter.com/1.1/statuses/home_timeline.json?' + params;
+
   const request = {
-    url: 'https://api.twitter.com/1.1/statuses/home_timeline.json',
+    url: url,
     method: 'GET',
   };
 
-  const oauth = await getOauthHeader(request);
+  const oauthHeader = await getOauthHeader(request);
 
   try {
     const response = await fetch(request.url, {
       method: 'GET',
       headers: {
         Host: 'api.twitter.com',
-        ...oauth,
+        ...oauthHeader,
       },
     });
     const json = await response.json();
